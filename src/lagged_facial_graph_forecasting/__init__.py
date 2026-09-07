39e006f7c7268af06249667c19fdedd0b19b50ba
shota5558 <7011apex@gmail.com>
2026-09-08 01:33:47 +0900

    P0A: initialize research package

---
+++ src/lagged_facial_graph_forecasting/__init__.py
@@ -0,0 +1,5 @@
+"""Lagged Facial Graph Forecasting research package."""
+
+from .scientific_config import ScientificConfigError, load_scientific_config
+
+__all__ = ["ScientificConfigError", "load_scientific_config"]
