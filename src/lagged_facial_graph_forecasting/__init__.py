2cbc80c6c7186614ae21ac9ab313f187ce744ef4
shota5558 <7011apex@gmail.com>
2026-09-08 01:38:19 +0900

    P0B-02: export SplitManifest contract

--- src/lagged_facial_graph_forecasting/__init__.py
+++ src/lagged_facial_graph_forecasting/__init__.py
@@ -1,11 +1,13 @@
 """Lagged Facial Graph Forecasting research package."""
 
-from .contracts import ContractError, FaceTimeSeries
+from .contracts import ContractError, FaceTimeSeries, InnerFold, SplitManifest
 from .scientific_config import ScientificConfigError, load_scientific_config
 
 __all__ = [
     "ContractError",
     "FaceTimeSeries",
+    "InnerFold",
+    "SplitManifest",
     "ScientificConfigError",
     "load_scientific_config",
 ]
