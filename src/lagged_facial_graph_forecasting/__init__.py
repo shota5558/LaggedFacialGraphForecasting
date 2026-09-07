28aa4c54d176c7db7b96e6528a8d624ca1b18ece
shota5558 <7011apex@gmail.com>
2026-09-08 01:36:44 +0900

    P0B-01: export FaceTimeSeries contract

--- src/lagged_facial_graph_forecasting/__init__.py
+++ src/lagged_facial_graph_forecasting/__init__.py
@@ -1,5 +1,11 @@
 """Lagged Facial Graph Forecasting research package."""
 
+from .contracts import ContractError, FaceTimeSeries
 from .scientific_config import ScientificConfigError, load_scientific_config
 
-__all__ = ["ScientificConfigError", "load_scientific_config"]
+__all__ = [
+    "ContractError",
+    "FaceTimeSeries",
+    "ScientificConfigError",
+    "load_scientific_config",
+]
